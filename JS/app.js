b62c1ee5768b8205b9e54e1a6152b6c87659c1e8
Mahmoud Easa <mu01011422865@gmail.com>
2022-01-12 21:59:23 +0200

    Add JS File

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+
+const buttonUp = document.querySelector(".button i");
+
+window.onscroll = (()=> {
+    if (document.body.scrollTop > 150 ||
+        document.documentElement.scrollTop > 150) {
+        buttonUp.style.display = "inline-flex";
+    } else {
+        buttonUp.style.display = "none";
+    }
+});
