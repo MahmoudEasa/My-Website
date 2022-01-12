e1175563a0cb22f8f02cb25b50de9e0ba9808d89
Mahmoud Easa <mu01011422865@gmail.com>
2022-01-12 23:49:23 +0200

    Edit my site 3

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@ const buttonUp = document.querySelector(".button i");
 window.onscroll = (()=> {
     if (document.body.scrollTop > 150 ||
         document.documentElement.scrollTop > 150) {
-        buttonUp.style.display = "inline-flex";
+        buttonUp.style.display = "flex";
     } else {
         buttonUp.style.display = "none";
     }
